c34a8a8e2c5041c6fb58e83ed429185220293a44
oomo3069 <oomo1212312121@gmail.com>
2025-07-21 17:18:55 +0700

    yeah

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -9,6 +9,8 @@ const pool = new Pool({
   database: process.env.DB_NAME,
   password: process.env.DB_PASSWORD,
   port: parseInt(process.env.DB_PORT || '5432', 10),
+  ssl: {
+    rejectUnauthorized: false // 💡 ต้องใช้ false เพราะ Render ใช้ใบรับรองจาก CA ที่ไม่เป็นทางการ
+  }
 });
-
 export default pool;
