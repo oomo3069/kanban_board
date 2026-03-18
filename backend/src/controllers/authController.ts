e9419b442642931d9bb3e4cdd6114593ce0bdf68
oomo3069 <oomo1212312121@gmail.com>
2026-03-18 22:42:45 +0700

    reset

--- backend/src/controllers/authController.ts
+++ backend/src/controllers/authController.ts
@@ -29,7 +29,7 @@ export const forgotPassword = async (req: Request, res: Response) => {
     }
     
     const token = jwt.sign({ id: user.id }, JWT_SECRET, { expiresIn: "10m" });
-    const resetLink = `https://kanban-board-chi-olive.vercel.app/reset-password/${token}`;
+    const resetLink = `https://kanban-board-eight-lyart.vercel.app/reset-password/${token}`;
 
 
     await transporter.sendMail({
